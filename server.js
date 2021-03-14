9618cbc278de0db81ecc19bf94bec4775788aab4
nottiffchan <nottiffchan@gmail.com>
2021-03-15 00:05:28 +0800

    change package and server details

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ var server = express();
 const path = require('path');
 const serveStatic = require('serve-static');
 const port = process.env.PORT || 8888;
-server.use(serveStatic(__dirname + '/dist/vibing'))
+// server.use(serveStatic(__dirname + '/dist/vibing'))
+server.use(express.static('./dist/vibing'));
+
+server.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname, '/dist/vibing/index.html'));
+});
 
 server.listen(port, () => console.log(`Server started at port ${port}!`));
 
